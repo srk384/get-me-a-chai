c69262d20e15f220842f2b0767f1ba54dbc62e19
srk384 <khanshahrukh384@gmail.com>
2025-01-15 16:16:18 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,13 +20,13 @@ export default function Home() {
     }
   }
 
-  if (session === undefined) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-      </div>
-    );
-  }
+  // if (session === undefined) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen">
+  //       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
+  //     </div>
+  //   );
+  // }
     return (
       <>
         <div className="min-h-[87.4vh] text-white">
